0bf1a45f9d94d1de024de9b2d245f12ff250215c
Maarten Engels <maartene@mac.com>
2019-04-05 21:09:15 +0200

    Initial Commit

---
+++ Hex Engine/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Hex Engine
+//
+//  Created by Maarten Engels on 05/04/2019.
+//  Copyright © 2019 thedreamweb. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
