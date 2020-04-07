6aa00587d8c1bbdaea42cebc0ba1eed74d9cf716
Maarten Engels <maartene@mac.com>
2020-04-07 12:10:00 +0200

    updated tile sprites. animation for movement.

---
+++ Hex Engine/Views/Sprites/LensSprite.swift
@@ -0,0 +1,13 @@
+//
+//  LensSprite.swift
+//  Hex Engine
+//
+//  Created by Maarten Engels on 06/04/2020.
+//  Copyright © 2020 thedreamweb. All rights reserved.
+//
+
+import Cocoa
+
+class LensSprite: SKSpriteNode {
+
+}
